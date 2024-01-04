be9576ebf1551f079f9518e9fe1f263b44f43255
giufalcao <thegiuliacavalcanti@gmail.com>
2024-01-04 18:06:30 -0300

    -  changing the approach to make the pipeline work locally

--- includes/configurations.py
+++ includes/configurations.py
@@ -1,3 +1,6 @@
 data_folder_path = "../data"
-processed_data_folder_path = "../processed_data"
+processed_folder_path = "../processed"
 presentation_folder_path = "../presentation"
+
+# FOR LOCAL PURPOSES
+warehouse_location = "../database/spark-warehouse"
