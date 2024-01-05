e010c406b59a28a7cb91d7662c091527a72b990a
giufalcao <thegiuliacavalcanti@gmail.com>
2024-01-05 16:42:31 -0300

    - preparing configurations for databricks notebooks

--- includes/configurations.py
+++ includes/configurations.py
@@ -2,5 +2,3 @@ data_folder_path = "../data"
 processed_folder_path = "../processed"
 presentation_folder_path = "../presentation"
 
-# FOR LOCAL PURPOSES
-warehouse_location = "../database/spark-warehouse"
