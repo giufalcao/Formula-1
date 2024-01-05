3bd764bc2d55d9da082d8b194111c6fc556002ca
giufalcao <thegiuliacavalcanti@gmail.com>
2024-01-05 18:07:51 -0300

    - migrating constructors file to Databricks Notebook

---
+++ ingestion/3.ingest_constructors_file.py
@@ -0,0 +1,92 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### Ingest constructors.json file
+
+# COMMAND ----------
+
+dbutils.widgets.text("p_data_source", "")
+v_data_source = dbutils.widgets.get("p_data_source")
+
+# COMMAND ----------
+
+dbutils.widgets.text("p_file_date", "2021-03-21")
+v_file_date = dbutils.widgets.get("p_file_date")
+
+# COMMAND ----------
+
+# MAGIC %run "../includes/configuration"
+
+# COMMAND ----------
+
+# MAGIC %run "../includes/common_functions"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 1 - Read the JSON file using the spark dataframe reader
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+
+# COMMAND ----------
+
+constructors_schema = StructType([
+    StructField("constructorId", IntegerType(), True),
+    StructField("constructorRef", StringType(), True),
+    StructField("name", StringType(), True),
+    StructField("nationality", StringType(), True),
+    StructField("url", StringType(), True)
+])
+
+# COMMAND ----------
+
+constructor_df = spark.read \
+    .schema(constructors_schema) \
+    .json(f"{raw_folder_path}/{v_file_date}/constructors.json")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 2 - Drop unwanted columns from the dataframe
+
+# COMMAND ----------
+
+columns_to_drop = ['url']
+
+# COMMAND ----------
+
+constructor_df = drop_columns(constructor_df, columns_to_drop)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 3 - Rename columns and add ingestion date
+
+# COMMAND ----------
+
+constructor_column_mapping = {
+    "constructorId": "constructor_id",
+    "constructorRef": "constructor_ref"
+}
+
+# COMMAND ----------
+
+constructor_df = rename_columns(constructor_df, constructor_column_mapping)
+
+# COMMAND ----------
+
+constructor_df = add_ingestion_date(constructor_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 4 Write output to parquet file
+
+# COMMAND ----------
+
+constructor_df.write.mode("overwrite").parquet(f"{processed_folder_path}/constructors")
+
+# COMMAND ----------
+
+dbutils.notebook.exit("Success")
